472947948960fc2750dc4c018adec8c0b7ea4f3a
rosalindc2 <rosalindc2@nycstudents.net>
2024-09-25 13:33:06 +0000

    1

---
+++ Scoreboard.java
@@ -0,0 +1,13 @@
+public class Scoreboard
+{
+    private String teamOne;
+    private String teamTwo;
+
+public Scoreboard(String one, String two)
+{
+    teamOne = one;
+    teamTwo = two;
+}
+public String getScore()
+}
+
